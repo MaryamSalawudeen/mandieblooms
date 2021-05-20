24bc5f2492d3a89012b7e5231c3d3133205dadae
maryam salawudeen <maryam.salawudeen@georgebrown.ca>
2021-05-20 01:36:35 -0400

    list-order2

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -54,7 +54,7 @@ export default function OrderListScreen(props) {
             </tr>
           </thead>
           <tbody>
-            {orders.map((order) => (
+          {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
                 <td>{order.user.name}</td>
