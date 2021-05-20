c7af3c42b69a433cb566717c8ef6ae1881f42fd7
maryam salawudeen <maryam.salawudeen@georgebrown.ca>
2021-05-19 22:03:37 -0400

    list-orders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 
 function App() {
@@ -108,6 +109,7 @@ const userSignin = useSelector((state) => state.userSignin);
             <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
             <PrivateRoute path="/profile" component={ProfileScreen}></PrivateRoute>
             <AdminRoute path="/productlist" component={ProductListScreen}></AdminRoute>
+            <AdminRoute path="/orderlist" component={OrderListScreen}></AdminRoute>
             <Route path="/" component = {HomeScreen} exact></Route> 
     
         </main>
