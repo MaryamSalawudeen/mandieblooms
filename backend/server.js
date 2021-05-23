88e636026e0f2698c665a26e720338bbdbd7f18b
maryam salawudeen <maryam.salawudeen@georgebrown.ca>
2021-05-22 21:46:32 -0400

    live-chat

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import { Server} from 'socket.io';
+import { Server } from 'socket.io';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -9,17 +9,17 @@ import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
 
-
 dotenv.config();
+
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mandieblooms', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+app.use(express.urlencoded({ extended: true }));
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mandieblooms', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -32,9 +32,6 @@ app.get('/api/config/google', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-
-
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
@@ -43,11 +40,12 @@ app.get('*', (req, res) =>
 //   res.send('Server is ready');
 // });
 
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+
 const port = process.env.PORT || 5000;
+
 const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 const users = [];
@@ -123,3 +121,7 @@ io.on('connection', (socket) => {
 httpServer.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
+
+// app.listen(port, () => {
+//   console.log(`Serve at http://localhost:${port}`);
+// });
